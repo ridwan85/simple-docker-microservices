bebbd531d40761b8a633b5de3f5e8d8a866a1c78
ridwan <rdwn.dev@gmail.com>
2019-12-06 23:49:43 +0800

    changed run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ docker-compose up -d  --force-recreate --build
 sleep 5 # wait until server is up ( wait for 5 seconds atleast)
 npm test # run unit test
 export $(xargs <.env)
-echo "The api server is currently running on http://localhost:${API_PORT}/api/v1/"
+echo "The api server is currently running on http://${HOST}:${API_PORT}/api/v1/"
